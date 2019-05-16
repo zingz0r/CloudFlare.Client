f4439f8599bda5bcae4fb9ecfac5e2519aca4bd5
zingz0r <zingz0r@tothnet.hu>
2019-05-16 17:22:07 +0200

    environment vars

--- CloudFlare.Client.Test/Credentials/Credentials.cs
+++ CloudFlare.Client.Test/Credentials/Credentials.cs
@@ -5,9 +5,9 @@ namespace CloudFlare.Client.Test.Credentials
 {
     public static class Credentials
     {
-        private static string ApiKey => Environment.GetEnvironmentVariable("CloudFlare.ApiKey");
+        private static string ApiKey => Environment.GetEnvironmentVariable("CloudFlare-ApiKey");
 
-        private static string EmailAddress => Environment.GetEnvironmentVariable("CloudFlare.EmailAddress");
+        private static string EmailAddress => Environment.GetEnvironmentVariable("CloudFlare-EmailAddress");
 
         public static Authentication Authentication => new Authentication(EmailAddress, ApiKey);
     }
