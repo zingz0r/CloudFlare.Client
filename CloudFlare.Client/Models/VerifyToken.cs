4eece9ae53afa7df83e2adfe9d33295ba3cac1f2
Selçuk Ermaya <selcukermaya@gmail.com>
2019-08-28 14:03:16 +0300

    Added ApiToken feature and tests.

---
+++ CloudFlare.Client/Models/VerifyToken.cs
@@ -0,0 +1,20 @@
+using CloudFlare.Client.Enumerators;
+using Newtonsoft.Json;
+
+namespace CloudFlare.Client.Models
+{
+    public class VerifyToken
+    {
+        /// <summary>
+        /// Membership identifier tag
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Status of this membership
+        /// </summary>
+        [JsonProperty("status")]
+        public VerifyTokenStatus Status { get; set; }
+    }
+}
