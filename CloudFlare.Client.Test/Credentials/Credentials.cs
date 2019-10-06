2195305ee23253f984bee2ec6d6a30de7667231c
zingz0r <zingz0r@tothnet.hu>
2019-10-06 13:57:51 +0200

    s

--- CloudFlare.Client.Test/Credentials/Credentials.cs
+++ CloudFlare.Client.Test/Credentials/Credentials.cs
@@ -5,11 +5,11 @@ namespace CloudFlare.Client.Test.Credentials
 {
     public static class Credentials
     {
-        private static string ApiKey => "1f009bf93a142d2cd7ff8b4adcd137e6edc09"; //Environment.GetEnvironmentVariable("ApiKey", EnvironmentVariableTarget.Process);
+        private static string ApiKey => Environment.GetEnvironmentVariable("ApiKey", EnvironmentVariableTarget.Process);
 
         private static string ApiToken => Environment.GetEnvironmentVariable("ApiToken", EnvironmentVariableTarget.Process);
 
-        private static string EmailAddress => "zgmode@gmail.com"; //Environment.GetEnvironmentVariable("EmailAddress", EnvironmentVariableTarget.Process);
+        private static string EmailAddress => Environment.GetEnvironmentVariable("EmailAddress", EnvironmentVariableTarget.Process);
 
         public static Authentication Authentication => new Authentication(EmailAddress, ApiKey, ApiToken);
     }
