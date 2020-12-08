6de193b587dd67880ba3ecd8552c1be776174d3e
Tamás Tóth <zingz0r@tothnet.hu>
2020-12-08 16:56:49 +0100

    refact http client extensions

--- CloudFlare.Client/Extensions/HttpClientExtensions.cs
+++ CloudFlare.Client/Extensions/HttpClientExtensions.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Security.Authentication;
 using System.Threading;
 using System.Threading.Tasks;
 using CloudFlare.Client.Api.Result;
 using CloudFlare.Client.Exceptions;
-using Newtonsoft.Json;
 
 namespace CloudFlare.Client.Extensions
 {
@@ -18,14 +14,14 @@ namespace CloudFlare.Client.Extensions
             CancellationToken cancellationToken = default)
         {
             var response = await httpClient.GetAsync(requestUri, cancellationToken).ConfigureAwait(false);
-            return await GetCloudFlareResultAsync<T>(response).ConfigureAwait(false);
+            return await response.GetCloudFlareResultAsync<T>().ConfigureAwait(false);
         }
 
         public static async Task<CloudFlareResult<T>> DeleteAsync<T>(this HttpClient httpClient, string requestUri,
             CancellationToken cancellationToken = default)
         {
             var response = await httpClient.GetAsync(requestUri, cancellationToken).ConfigureAwait(false);
-            return await GetCloudFlareResultAsync<T>(response).ConfigureAwait(false);
+            return await response.GetCloudFlareResultAsync<T>().ConfigureAwait(false);
         }
 
         public static async Task<CloudFlareResult<T>> PatchAsync<T>(this HttpClient httpClient, string requestUri,
@@ -37,7 +33,7 @@ namespace CloudFlare.Client.Extensions
                 var request = new HttpRequestMessage(method, requestUri) { Content = content };
 
                 var response = await httpClient.SendAsync(request, cancellationToken).ConfigureAwait(false);
-                return await GetCloudFlareResultAsync<T>(response).ConfigureAwait(false);
+                return await response.GetCloudFlareResultAsync<T>().ConfigureAwait(false);
 
             }
             catch (Exception ex)
@@ -50,36 +46,14 @@ namespace CloudFlare.Client.Extensions
             TContent content, CancellationToken cancellationToken = default)
         {
             var response = await httpClient.PostAsJsonAsync(requestUri, content, cancellationToken).ConfigureAwait(false);
-            return await GetCloudFlareResultAsync<TResult>(response).ConfigureAwait(false);
+            return await response.GetCloudFlareResultAsync<TResult>().ConfigureAwait(false);
         }
 
         public static async Task<CloudFlareResult<TResult>> PutAsync<TResult, TContent>(this HttpClient httpClient, string requestUri,
             TContent content, CancellationToken cancellationToken = default)
         {
             var response = await httpClient.PutAsJsonAsync(requestUri, content, cancellationToken).ConfigureAwait(false);
-            return await GetCloudFlareResultAsync<TResult>(response).ConfigureAwait(false);
-        }
-
-        private static async Task<CloudFlareResult<T>> GetCloudFlareResultAsync<T>(HttpResponseMessage response)
-        {
-            try
-            {
-                var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                switch (response.StatusCode)
-                {
-                    case HttpStatusCode.Forbidden:
-                    case HttpStatusCode.Unauthorized:
-                        var errorResult = JsonConvert.DeserializeObject<CloudFlareResult<object>>(content);
-                        throw new AuthenticationException(string.Join(Environment.NewLine, errorResult.Errors.Select(x => x.Message)));
-                    default:
-                        return JsonConvert.DeserializeObject<CloudFlareResult<T>>(content);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new PersistenceUnavailableException(ex);
-            }
+            return await response.GetCloudFlareResultAsync<TResult>().ConfigureAwait(false);
         }
     }
 }
