cd6926c5baad7da6a57dafe26552b0209a812d07
zingz0r <zingz0r@tothnet.hu>
2020-12-10 01:08:20 +0100

    dns record tests

--- CloudFlare.Client.Test/ClientTests/DnsRecordUnitTests.cs
+++ CloudFlare.Client.Test/ClientTests/DnsRecordUnitTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using CloudFlare.Client.Enumerators;
 using FluentAssertions;
 using Xunit;
 
@@ -8,12 +10,54 @@ namespace CloudFlare.Client.Test.ClientTests
     public class DnsRecordUnitTests
     {
         [Fact]
-        public async Task CreateDelete()
+        public async Task TestCreateDnsRecordAsyncDeleteDnsRecordAsync()
         {
             using var client = new CloudFlareClient(Credentials.Credentials.Authentication);
             var zone = (await client.GetZonesAsync()).Result.First();
 
-            false.Should().BeTrue();
+            var create = await client.CreateDnsRecordAsync(zone.Id, DnsRecordType.A, $"{Guid.NewGuid()}.{zone.Name}", "1.1.1.1");
+            var delete = await client.DeleteDnsRecordAsync(zone.Id, create.Result.Id);
+
+            create.Success.Should().BeTrue();
+            create.Errors?.Should().BeEmpty();
+            delete.Success.Should().BeTrue();
+            delete.Errors?.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task TestExportDnsRecordsAsync()
+        {
+            using var client = new CloudFlareClient(Credentials.Credentials.Authentication);
+            var zone = (await client.GetZonesAsync()).Result.First();
+
+            var export = await client.ExportDnsRecordsAsync(zone.Id);
+
+            export.Success.Should().BeTrue();
+            export.Errors?.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task TestGetDnsRecordsAsync()
+        {
+            using var client = new CloudFlareClient(Credentials.Credentials.Authentication);
+            var zone = (await client.GetZonesAsync()).Result.First();
+            var records = (await client.GetDnsRecordsAsync(zone.Id));
+
+            records.Success.Should().BeTrue();
+            records.Errors?.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task TestGetDnsRecordDetailsAsync()
+        {
+            using var client = new CloudFlareClient(Credentials.Credentials.Authentication);
+            var zone = (await client.GetZonesAsync()).Result.First();
+            var record = (await client.GetDnsRecordsAsync(zone.Id)).Result.First();
+
+            var recordDetails = await client.GetDnsRecordDetailsAsync(zone.Id, record.Id);
+
+            recordDetails.Success.Should().BeTrue();
+            recordDetails.Errors?.Should().BeEmpty();
         }
 
         [Fact]
@@ -23,9 +67,21 @@ namespace CloudFlare.Client.Test.ClientTests
             var zone = (await client.GetZonesAsync()).Result.First();
             var scanZone = await client.ScanDnsRecordsAsync(zone.Id);
 
-            scanZone.Should().NotBeNull();
             scanZone.Errors?.Should().BeEmpty();
             scanZone.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task TestUpdateDnsRecordAsync()
+        {
+            using var client = new CloudFlareClient(Credentials.Credentials.Authentication);
+            var zone = (await client.GetZonesAsync()).Result.First();
+            var record = (await client.GetDnsRecordsAsync(zone.Id)).Result.First();
+
+            var update = await client.UpdateDnsRecordAsync(zone.Id, record.Id, record.Type, record.Name, record.Content);
+
+            update.Errors?.Should().BeEmpty();
+            update.Success.Should().BeTrue();
         }
     }
 }
