e38385ae38117dc6370ecd027776e1b6aa671a98
zingz0r <Tamas_Toth2@epam.com>
2021-01-12 16:18:24 +0100

    use mmstream if its not targeted .netstandard 2.0

--- CloudFlare.Client/Helpers/FileHelper.cs
+++ CloudFlare.Client/Helpers/FileHelper.cs
@@ -8,10 +8,15 @@ namespace CloudFlare.Client.Helpers
     {
         public static async Task<byte[]> ReadAsync(string path, CancellationToken cancellationToken)
         {
-            using var sourceStream = File.Open(path, FileMode.Open);
-
+            await using var sourceStream = File.Open(path, FileMode.Open);
             var result = new byte[sourceStream.Length];
-            await sourceStream.ReadAsync(result, 0, (int)sourceStream.Length, cancellationToken).ConfigureAwait(false);
+
+#if NETSTANDARD2_0
+            await sourceStream.ReadAsync(result, 0, (int)sourceStream.Length, cancellationToken);
+#else
+            await using var memoryStream = new MemoryStream(result);
+            await sourceStream.ReadAsync(memoryStream.GetBuffer(), cancellationToken);
+#endif
 
             return result;
         }
