e9748c110c29c9a4c7fbfaf01c9c62066eefe02a
zingz0r <Tamas_Toth2@epam.com>
2021-01-13 21:16:54 +0100

    subscription tests

---
+++ CloudFlare.Client.Test/TestData/SubscriptionTestData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using CloudFlare.Client.Api.Accounts.Subscriptions;
+using CloudFlare.Client.Enumerators;
+
+namespace CloudFlare.Client.Test.TestData
+{
+    public static class SubscriptionTestData
+    {
+        public static List<Subscription> Subscriptions { get; set; } = new()
+        {
+            new Subscription
+            {
+                App = new App { InstallId = null },
+                ComponentValues = new List<ComponentValue>
+                {
+                    new() {Name = ComponentValueType.PageRules, Default = 3, Price = 0, Value = 0}
+                },
+                Currency = "USD",
+                CurrentPeriodStart = DateTime.UtcNow,
+                Frequency = Frequency.Monthly,
+                Id = Guid.NewGuid().ToString(),
+                Price = 0,
+                RatePlan = new RatePlan
+                {
+                    Id = LegacyType.Free,
+                    Currency = "USD",
+                    ExternallyManaged = false,
+                    IsContract = false,
+                    PublicName = "Test RatePlan",
+                    Scope = RatePlanScope.Zone,
+                    Sets = new List<string>()
+                },
+                State = SubscriptionState.Paid
+            }
+        };
+    }
+}
