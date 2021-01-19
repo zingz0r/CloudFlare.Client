40dc0870a7147a5c8de264e2e9b66e7a4a45a480
zingz0r <Tamas_Toth2@epam.com>
2021-01-19 09:43:30 +0100

    fix key rename

--- CloudFlare.Client.Test/WireMockConnection.cs
+++ CloudFlare.Client.Test/WireMockConnection.cs
@@ -7,9 +7,9 @@ namespace CloudFlare.Client.Test
     public class WireMockConnection
     {
         public static string EmailAddress { get; } = "test@tothnet.hu";
-        public static string Password { get; } = "p@ssw0rd";
+        public static string Key { get; } = "p@ssw0rd";
         public static string Token { get; } = "8M7wS6hCpXVc-DoRnPPY_UCWPgy8aea4Wy6kCe5T";
-        public static ApiKeyAuthentication ApiKeyAuthentication { get; } = new(EmailAddress, Password);
+        public static ApiKeyAuthentication ApiKeyAuthentication { get; } = new(EmailAddress, Key);
         public static ApiTokenAuthentication ApiTokenAuthentication { get; } = new(Token);
 
         public ConnectionInfo ConnectionInfo { get; }
