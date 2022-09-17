ac29499558c23243e69e984cfebe41b9a9730b15
Hector Gustavo Che Calderon <hectorchec@outlook.com>
2022-09-17 01:34:14 -0500

    DNSRecordFilter Match parameter
    Implementing all | any match parameter for filtering on DNSRecords Filter Request

---
+++ CloudFlare.Client/Enumerators/DnsRecordFilterMatchType.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using CloudFlare.Client.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CloudFlare.Client.Enumerators
+{
+    /// <summary>
+    /// Represents how to match filters
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum DnsRecordFilterMatchType
+    {
+        /// <summary>
+        /// All
+        /// </summary>
+        [EnumMember(Value = "all")]
+        All,
+
+        /// <summary>
+        /// Any
+        /// </summary>
+        [EnumMember(Value = "any")]
+        Any
+    }
+}
