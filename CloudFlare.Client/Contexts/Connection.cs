f120382551134a645a5a0dfefbfe618e3eadd53c
zingz0r <zingz0r@tothnet.hu>
2024-12-27 21:11:28 +0100

    sarif move

--- CloudFlare.Client/Contexts/Connection.cs
+++ CloudFlare.Client/Contexts/Connection.cs
@@ -2,7 +2,6 @@
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
